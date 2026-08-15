4c72dd71f671bd4ba30a58ac70ff4e6cb957a741
penumutchusairakesh-tel <penumutchu.sairakesh@gmail.com>
2026-08-15 17:05:38 +0530

    added first digit puzzle

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ inventory = []
 lifes = 5
 main_door_password = "3724051"
 main_door_unlocked = False
+found_digits = []
+#_________________________________________________________
 def main_door():
-    global main_door_unlocked
+    global main_door_unlocked , lifes
     print("The main door has a 7 digit Lock\n_ _ _ _ _ _ _\nNOTE: YOU NEED ALL THE & DIGITS TOGETHER TO ESCAPE!!")
     print("Enter 1. to enter the password 2.Exit searching password")
     PASSWORD_ENTER_CHOICE = int(input())
@@ -20,6 +22,23 @@ def main_door():
     else:
         print("GOING BACK TO THE ROOMS")
         return
+#_________________________________________________________
+def check_drawer():
+    # global inventory (not needed since i am just modifying)
+    if "drawer_key" not in inventory: #if drawer key is not there
+        print("You need a key to open the door")
+        return
+    else: #if key is there
+        print("Drawer Key USED!")
+        inventory.remove("drawer_key")
+        print("FOUND A PAPER -> \"1\"")
+        found_digits.append(1) 
+#_________________________________________________________
+def under_couch():
+    print("Found a drawer key!! ADDED TO INVENTORY")
+    inventory.append("drawer_key")
+#_________________________________________________________
+
     
 print("YOU ARE IN A ROOM WHICH U HAVE TO ESCAPE : YOU HAVE 5 LIFES PLAY WISELY ")
 while not(main_door_unlocked) and lifes != 0: #even if one is false game ends
@@ -29,9 +48,34 @@ while not(main_door_unlocked) and lifes != 0: #even if one is false game ends
           2. CHECK INSIDE THE HOUSE DRAWER
           3. CHECK INDISE THE HOUSE CUPBOARD
           4. CHECK UNDER THE COUCH
-          4. ENTER THE KITCHEN
-          5. ENTER THE BEDROOM
-          6. ENTER THE BATHROOM
+          5. ENTER THE KITCHEN
+          6. ENTER THE BEDROOM
+          7. CHECK INVENTORY
+          8. CHECK PASSWORD DIGITS FOUND
           """)
-     
+    USER_CHOICE = int(input())
+    match USER_CHOICE:
+        case 1:
+            main_door()
+        case 2:
+            check_drawer()
+        case 3:
+            pass
+        case 4:
+            under_couch()
+        case 5:
+            pass
+        case 6:
+            pass
+        case 7:
+            print("YOU HAVE ")
+            for index in range((len(inventory))): #0 to len-1 (since range excludes the extreme end)
+                print(inventory[index],end=" ")
+        case 8:
+            print("YOU HAVE FOUND")
+            for index in range((len(found_digits))):
+                print(found_digits[index],end=" ")
+            
+            
+            
     
